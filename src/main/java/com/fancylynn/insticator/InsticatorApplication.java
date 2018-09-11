d52d62a1ee93019db0a648a5a3e9bddee463ca8d
Fancylynn <fancylynn0@gmail.com>
2018-09-10 21:35:49 -0400

    initial commit

---
+++ src/main/java/com/fancylynn/insticator/InsticatorApplication.java
@@ -0,0 +1,12 @@
+package com.fancylynn.insticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsticatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fancylynn/insticator/InsticatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fancylynn.insticator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InsticatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
